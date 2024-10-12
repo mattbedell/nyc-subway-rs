772be6ad1d2b147873c477f6de10c0df5a882d1f
Matt Bedell <matt@bedell.co>
2024-10-12 17:19:10 -0400

    Macro impl Deserialize opt enum discriminant

--- nyc_subway_rs_derive/src/lib.rs
+++ nyc_subway_rs_derive/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro_error::{proc_macro_error, Diagnostic, Level};
-use quote::{format_ident, quote};
-use syn::{Arm, Attribute, Data, Expr, ExprMatch, Pat, Variant};
+use quote::{quote, format_ident};
+use syn::Data;
 
 #[proc_macro_derive(Deserialize_enum_or, attributes(fallback))]
 pub fn deserialize_enum_or_macro_derive(input: TokenStream) -> TokenStream {
@@ -29,17 +29,44 @@ fn impl_deserialize_enum(ast: &syn::DeriveInput) -> TokenStream {
             .ident;
         let variants = data.variants.iter().map(|variant| &variant.ident);
         let disc = data.variants.iter().map(|variant| &variant.discriminant.as_ref().unwrap().1);
+        let visitor_name = format_ident!("My{}Visitor", name);
         let gen = quote! {
-            impl #name {
-                fn print_name() {
-                    println!("Macro dervice impl for: {}", stringify!(#name));
-                }
+            impl<'de> Deserialize<'de> for #name {
+                fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+                where
+                    D: Deserializer<'de>,
+                {
+                    struct #visitor_name {}
+                    impl<'de> Visitor<'de> for #visitor_name {
+                        type Value = #name;
 
-                fn do_match(input: u16) -> #name {
-                    match input {
-                        #(#disc => #name::#variants),*,
-                        _ => #name::#fallback_variant,
+                        fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+                            write!(formatter, "an integer between 0 and {}", u8::MAX)
+                        }
+
+                        fn visit_u8<E>(self, v: u8) -> std::result::Result<Self::Value, E>
+                            where
+                                E: serde::de::Error, {
+                            match v {
+                                #(#disc => Ok(Self::Value::#variants)),*,
+                                _ => Ok(Self::Value::#fallback_variant),
+                            }
+                        }
+
+                        fn visit_none<E>(self) -> std::result::Result<Self::Value, E>
+                            where
+                                E: serde::de::Error, {
+                            Ok(Self::Value::#fallback_variant)
+                        }
+
+                        fn visit_some<D>(self, deserializer: D) -> std::result::Result<Self::Value, D::Error>
+                            where
+                                D: Deserializer<'de>, {
+                            deserializer.deserialize_u8(self)
+                        }
                     }
+
+                    deserializer.deserialize_option(#visitor_name {})
                 }
             }
         };
